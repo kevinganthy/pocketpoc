c98fdfe25303adb7f433c5163ef65b31799c12ba
Kevin <kevin.ganthy@oclock.io>
2024-11-20 19:58:25 +0100

    [no ci] responsive

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -5,13 +5,13 @@ const Login: Component = () => {
   return (
     <div class="h-screen w-screen flex items-center justify-center bg-slate-800">
       
-      <div class="flex gap-32 bg-slate-50 rounded-lg p-12 pt-24">
+      <div class="flex flex-wrap lg:flex-nowrap w-11/12 max-w-3xl gap-4 lg:gap-32 bg-slate-50 rounded-lg p-4 lg:p-12 pt-8 lg:pt-24">
         <div class="flex flex-col gap-2">
           <h1 class="font-display uppercase text-primary text-4xl">{import.meta.env.VITE_APP_NAME}</h1>
-          <p class="text-lg text-gray-500 w-64">{import.meta.env.VITE_APP_DESCRIPTION}</p>
+          <p class="text-lg text-gray-500 grow">{import.meta.env.VITE_APP_DESCRIPTION}</p>
         </div>
 
-        <div class="p-12 pt-0">
+        <div class="w-full p-4 pt-12 lg:p-12 lg:pt-0">
           <LoginForm />
         </div>
       </div>
