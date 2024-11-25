0f625d404d40ee0491a340454689422fd9bab2a8
Kevin <kevin.ganthy@oclock.io>
2024-11-25 08:29:01 +0100

    Implement dynamic OAuth provider buttons and improve loading state in LoginOauth component

--- front/src/components/LoginOauth.tsx
+++ front/src/components/LoginOauth.tsx
@@ -1,19 +1,29 @@
-import { Component } from "solid-js";
+import { Component, createSignal, onMount, Show, For } from "solid-js";
 import { useNavigate } from "@solidjs/router";
 import { usePb } from "../components/context/PbContext";
 import toast from "solid-toast";
 
+interface Provider {
+    authUrl :string;
+    codeChallenge :string;
+    codeChallengeMethod :string;
+    codeVerifier :string;
+    displayName :string;
+    name :string;
+    state :string;
+}
 
 const LoginOauth: Component = () => {
     const pb = usePb();
     const navigate = useNavigate();
+    const [providers, setProviders] = createSignal<Provider[]>([]);
+
+    onMount(async () => {
+        const providers = await pb.collection('users').listAuthMethods();
+        setProviders(providers.authProviders);
+    });
 
     const oAuth2 = async (provider :string) => {
-        if ( ["apple"].indexOf(provider) !== -1 ) {
-            toast.error("Provider non implémenté");
-            return;
-        }
-
         try {
             await pb.collection('users').authWithOAuth2({ provider: provider });
         } catch (e) {
@@ -26,19 +36,18 @@ const LoginOauth: Component = () => {
             toast.success("Connecté avec succès");
         }
     }
-
-      
+  
     return (
         <div class="flex flex-col gap-4 ">
-            <button onClick={[oAuth2, "github"]} class="btn btn-primary">
-              <i class="ph ph-github-logo"></i> Github
-            </button>
-            <button onClick={[oAuth2, "google"]} class="btn btn-outline">
-              <i class="ph ph-google-logo"></i> Google
-            </button>
-            <button onClick={[oAuth2, "apple"]} class="btn btn-outline">
-              <i class="ph ph-apple-logo"></i> Apple
-            </button>
+            <Show when={providers().length === 0}>
+                <p>No provider set, go to Pocketbase in order to add one.</p>
+            </Show>
+
+            <For each={providers()}>{(provider) =>
+                <button onClick={[oAuth2, provider.name]} class="btn btn-outline">
+                    <i class={`ph ph-${provider.name}-logo`}></i> {provider.displayName}
+                </button>
+            }</For>
         </div>
     );
 };
--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -17,12 +17,12 @@ const Login: Component = () => {
       <Toaster />
       
       <div class="flex flex-wrap lg:flex-nowrap w-11/12 max-w-3xl gap-4 lg:gap-16 bg-slate-50 rounded-lg p-4 lg:p-12 pt-8 lg:pt-24">
-        <div class="flex flex-col gap-2 w-full">
+        <div class="flex flex-col gap-2 pb-12 w-full">
           <h1 class="font-display uppercase text-primary text-4xl mt-2">{import.meta.env.VITE_APP_NAME}</h1>
           <p class="text-lg text-gray-500 grow">{import.meta.env.VITE_APP_DESCRIPTION}</p>
         </div>
 
-        <div class="w-full p-4 pt-12 lg:p-12 lg:pt-0">
+        <div class="w-full p-4 pt-12 lg:p-12 lg:pt-0 flex flex-col justify-center">
           <Show 
             when={useAuthProviders()}
             fallback={<LoginForm />}
